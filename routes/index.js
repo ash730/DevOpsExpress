959380a1e0810e8e155abf0384215c149718651e
ashleymills <ash730@gmail.com>
2021-10-08 13:43:29 -0400

    logger

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+const logger = require('../logger');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api/add', function(req, res, next){
+  var n1 = parseInt(req.query.n1);
+  var n2 = parseInt(req.query.n2);
+  logger.info('/api/add Used to add ' + n1 + ' and ' + n2);
+  res.json({answer: n1 + n2});
 });
 
 module.exports = router;
